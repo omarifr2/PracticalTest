c90da20608dd37ebe98748eaacabd923dd8fa8d3
omarifr <omar.franco@3pillarglobal.com>
2024-07-08 16:09:03 -0600

    modified case7

--- Case6/obj/Debug/net8.0/Case6.AssemblyInfo.cs
+++ Case6/obj/Debug/net8.0/Case6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Case6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9dd65b10a1ce5697c9b7058a1c8cc7b6d421e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Case6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Case6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Case7/Program.cs
+++ Case7/Program.cs
@@ -1,16 +1,19 @@
-﻿namespace  Case7;
+﻿//* Create a Button Class that contain the Click delegate. And receive it in the constructor.
+//* Create an instance of Button and name it saveButton.//     
+//    Inject the function Click.
+//    Function should write in the console "Saved". 
+//* Create a cancelButton. Do the same but write "Canceled"
+namespace Case7;
 
-public delegate void SendMessage(string message);
-
-
-//1.- Create a Messenger Class that contain the SendMessage delegate.
-//2.- Send a Email and SMS message using the Messenger Class
-
+public delegate void Click();
 
 class Program
 {
     static void Main()
     {
-        
+
     }
 }
+
+
+
